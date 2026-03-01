380ad194e5f3a629bb97d3c9998f6addf7d3d51d
muhammadjonsidiqjonov02-eng <muhammadjonsidiqjonov02@gmail.com>
2026-03-01 20:27:38 +0500

    Add files via upload

---
+++ check password.py
@@ -0,0 +1,8 @@
+ism = "Muhammadjon"
+for i in range(3):
+    user = input("Ismni kiriting :")
+    if user == ism :
+        print("Xush kelibsiz ")
+        break
+    else:
+        print("Xato,qayta uring")
